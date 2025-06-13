0498a755cef36391fe5a451629bfb3bc72d22f4e
AnasAhmedFND <anasahmed4402@gmail.com>
2025-06-13 18:04:21 +0600

    footer nad our store section done

--- src/Compunents/Footer.jsx
+++ src/Compunents/Footer.jsx
@@ -2,12 +2,67 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section>
-        <div className="py-20 bg-[#15032a] text-2xl text-white border text-center">
-            <h1>Footer</h1>
+    <section className=' bg-[#FFEDED] pt-10'>
+      {/* top div ****************************** */}
+      <div className="flex container mx-auto py-5 pb-10  justify-between">
+
+        {/* first div........... */}
+        <div className=" w-[25%]  ">
+          <h4 className='font-bold text-xl'>have a message?</h4>
+          <input className='border mt-4 py-2 rounded-md w-full bg-white text-[12px] ' type="text" placeholder='NAME*' />
+          <input className='border mt-2 py-2 rounded-md w-full bg-white text-[12px] ' type="text" placeholder='EMAIL*' />
+          <input className='border mt-2 py-2 rounded-md w-full bg-white text-[12px] ' type="text" placeholder='NUMBER*' />
+          <textarea className='border mt-2 py-2 rounded-md w-full bg-white text-[12px] ' name="message" id="sms" placeholder='YOUR MESSAGE*' >
+
+          </textarea>
+          <button className='font-bold text-[12px] mt-2 border py-2 px-4 bg-[#433E3E] text-white  '>SEND INQUIRES</button>
+
         </div>
 
-        <div className=" "></div>
+        {/* two .............*/}
+        <div className=" w-[24%] ">
+          <h4 className='font-bold text-xl '>contac us</h4>
+          <p className='mt-4 '>2215 US Highway 1 South, North Brunswick, NJ 08902</p>
+          <p className='mt-2 '>Call Us At</p>
+          <p className='mt-2 '>P : 732.398.9022</p>
+          <p className='mt-2 '>M: 917.518.4331</p>
+          <p className='mt-2 '>M: 347.784.9269</p>
+          <p className='mt-2 '>Fax: 732.658 3700</p>
+          <p className='mt-2 '>Email: gurupalace@hotmail.com</p>
+
+        </div>
+
+        {/* three ...............*/}
+        <div className=" w-[20%] ">
+          <h4 className='font-bold text-xl'>Qucik lines</h4>
+          <p className='mt-4'>Home</p>
+          <p className='mt-2'>Menu </p>
+          <p className='mt-2'>Banquet Facility</p>
+          <p className='mt-2'>Gallery</p>
+          <p className='mt-2'>Catering</p>
+          <p className='mt-2'>Evenls</p>
+          <p className='mt-2'>Contact Us</p>
+
+        </div>
+
+        {/*  four................*/}
+        <div className=" w-[24%] ">
+          <h4 className='font-bold text-xl'>Hours of opration</h4>
+          <p className='mt-4 '>6 DAYS OPEN</p>
+          <p className='mt-2 '>Monday, Wednesday-Friday : 11AM To 3PM</p>
+          <p className='mt-2 '>Monday, Wednesday-Friday : 5PM To 9:30PM</p>
+          <p className='mt-2 '>Saturday-Sunday : 10AM To 9:30PM</p>
+          <p className='mt-2 '>Tuesday: Closed</p>
+
+        </div>
+
+      </div>
+
+      {/* bottom div ******************** */}
+      <div className="h-[250px] text-white bg-black items-center flex justify-center border relative">
+        <img className='w-[250px] absolute -top-10 ' src="/Group2.png" alt="resturent-icon" />
+
+      </div>
     </section>
   )
 }
--- src/Compunents/Home_/Stories.jsx
+++ src/Compunents/Home_/Stories.jsx
@@ -1,22 +1,50 @@
+import { Inter } from 'next/font/google'
 import React from 'react'
+
+
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter"
+})
+
 
 const Stories = () => {
   return (
-    <section className='container mx-auto'>
-        <div className="flex ">
-            <div className="w-1/2 ">
-            <img className='h-[300px] ' src="/hero/stories.png" alt="" />
+    <section className='container mx-auto py-5 '
+      style={{ backgroundImage: "url('/hero/stores-bg.png')" }}
+    >
+      <div className="flex justify-between  " >
 
-            </div>
-
-            <div className="w-1/2 ">
-                <h3 className='font-bold '><span className='underline'>OUR </span>  STORIE</h3>
-                <p>At Guru Palace Indian Restaurant, we <br /> offer distinctive cuisine in an informal, <br /> friendly setting. Our restaurant enjoys a <br /> reputation for impeccable service, <br /> elegant Asian-style décor, and exquisite <br /> Indian food. Dining at our restaurant is <br /> like visiting India without leaving your <br /> hometown–an exciting blend of authentic <br /> Indian food, culture, and atmosphere <br /> awaits.</p>
-            </div>
+        <div className="w-[48%] ">
+          <img className='h-[380px] w-full  ' src="/hero/stories.png" alt="" />
 
         </div>
 
-        <div className=""></div>
+        <div className="w-[48%] ">
+          <h3 className='font-bold text-2xl text-red-500 tracking-widest'><span className='underline'>OUR </span>  STORIE</h3>
+          <p className={`${inter.className} mt-2 tracking-widest`}>At Guru Palace Indian Restaurant, we <br /> offer distinctive cuisine in an informal, <br /> friendly setting. Our restaurant enjoys a <br /> reputation for impeccable service, <br /> elegant Asian-style décor, and exquisite <br /> Indian food. Dining at our restaurant is <br /> like visiting India without leaving your <br /> hometown–an exciting blend of authentic <br /> Indian food, culture, and atmosphere <br /> awaits.</p>
+
+          <button className='font-bold text-white py-2 px-5 mt-5 bg-[#433E3E] '>KNOW ABOUT US</button>
+        </div>
+
+      </div>
+
+      <div className="flex justify-around mt-4">
+        <div className="text-center ">
+          <img className='w-[200px] h-[200px]  ' src="/hero/st1.png" alt="logo" />
+          <p className='tracking-widest text-green-500'> UBER <br /> EATS</p>
+
+        </div>
+
+        <img className='w-[200px] h-[200px]  ' src="/hero/st2.png" alt="logo" />
+
+        <div className=' text-center text-red-500 text-xl'>
+          <img className='w-[200px] h-[200px]  ' src="/hero/st3.png" alt="logo" />
+          <p className='tracking-widest '>support</p>
+
+        </div>
+      </div>
     </section>
   )
 }
